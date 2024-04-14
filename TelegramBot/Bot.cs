94fb770413cd22e93e5f21b106df25d30861c422
Linuxfight <kiko13024@gmail.com>
2024-04-14 20:53:33 +0300

    init

---
+++ TelegramBot/Bot.cs
@@ -0,0 +1,79 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+
+namespace TelegramBot;
+
+public class Bot
+{
+    private TelegramBotClient _bot;
+    private CancellationTokenSource _cts;
+
+    public Bot(string botToken)
+    {
+        _bot = new(botToken);
+        _cts = new();
+    }
+
+    public void Start()
+    {
+        ReceiverOptions receiverOptions = new ()
+        {
+            ThrowPendingUpdates = true
+        };
+
+        _bot.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: _cts.Token
+        );
+    }
+
+    public void Stop()
+    {
+        _cts.Cancel();
+        Environment.Exit(0);
+    }
+
+    public async Task GetInfo()
+    {
+        User bot = await _bot.GetMeAsync();
+        Console.WriteLine($"Start listening for @{bot.Username}");
+    }
+
+    private async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    {
+        // Only process Message updates: https://core.telegram.org/bots/api#message
+        if (update.Message is not { } message)
+            return;
+        // Only process text messages
+        if (message.Text is not { } messageText)
+            return;
+
+        long chatId = message.Chat.Id;
+
+        Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+        // Echo received message text
+        Message sentMessage = await botClient.SendTextMessageAsync(
+            chatId: chatId,
+            text: "You said:\n" + messageText,
+            cancellationToken: cancellationToken);
+    }
+
+    private Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        string? ErrorMessage = exception switch
+        {
+            ApiRequestException apiRequestException
+                => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+            _ => exception.ToString()
+        };
+
+        Console.WriteLine(ErrorMessage);
+        Environment.Exit(1);
+        return null;
+    }
+}
---
+++ TelegramBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using TelegramBot;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        string? botToken = Environment.GetEnvironmentVariable("BOT_TOKEN");
+        if (botToken == null)
+        {
+            Console.WriteLine("Bot token is not set");
+            Environment.Exit(1);
+        }
+        Bot bot = new(botToken);
+        bot.Start();
+        await bot.GetInfo();
+        Console.ReadLine();
+        bot.Stop();
+    }
+}
