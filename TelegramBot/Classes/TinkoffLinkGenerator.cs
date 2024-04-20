62cfaf9f90b0944b6bee29a1fabfc7507bb8d9a6
Linuxfight <kiko13024@gmail.com>
2024-04-20 20:57:48 +0300

    added testing items

--- TelegramBot/Classes/TinkoffLinkGenerator.cs
+++ TelegramBot/Classes/TinkoffLinkGenerator.cs
@@ -1,7 +1,4 @@
-﻿using System.Net.Http;
-using Newtonsoft.Json;
-
-namespace TelegramBot;
+﻿namespace TelegramBot.Classes;
 
 public static class TinkoffLinkGenerator
 {
@@ -15,7 +12,40 @@ public static class TinkoffLinkGenerator
             Amount = 140000,
             OrderId = "2109023781937921",
             Description = "Подарочная карта на 1000 рублей",
-            Token = "68711168852240a2f34b6a8b19d2cfbd296c7d2a6dff8b23eda6278985959346"
+            Token = "68711168852240a2f34b6a8b19d2cfbd296c7d2a6dff8b23eda6278985959346",
+            Receipt = new()
+            {
+                Email = "a@test.ru",
+                Phone = "+79031234567",
+                Items = new List<Items_FFD_105>()
+                {
+                    new()
+                    {
+                        Name = "Наименование товара 1",
+                        Price = 10000,
+                        Quantity = 1,
+                        Amount = 10000,
+                        Tax = Items_FFD_105Tax.Vat10,
+                        Ean13 = "303130323930303030630333435"
+                    },
+                    new()
+                    {
+                        Name = "Наименование товара 2",
+                        Price = 20000,
+                        Quantity = 2,
+                        Amount = 40000,
+                        Tax = Items_FFD_105Tax.Vat20
+                    },
+                    new()
+                    {
+                        Name = "Наименование товара 3",
+                        Price = 30000,
+                        Quantity = 3,
+                        Amount = 90000,
+                        Tax = Items_FFD_105Tax.Vat10
+                    }
+                }
+            }
         };
         return await client.InitAsync(init);
     }
